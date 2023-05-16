5a60965c2967cf3805845b1a6e5ef0be948830fb
Naji <lemiine987@gmail.com>
2023-05-16 15:36:07 +0300

    full code

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+var express = require("express");
+const multer = require("multer");
+var cors = require("cors");
+require("dotenv").config();
+
+const upload = multer();
 
 var app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-
-
+// handle file upload
+app.post("/api/fileanalyse", upload.single("upfile"), function (req, res) {
+  const fileName = req.file.originalname;
+  const fileType = req.file.mimetype;
+  const fileSize = req.file.size;
+  res.json({ name: fileName, type: fileType, size: fileSize });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
